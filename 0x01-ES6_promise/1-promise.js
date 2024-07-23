6924c74500ca3d830f4cb6f858227de3120e4bbc
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-23 18:09:19 +0300

    promise with obj returned and error handling

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const obj = {
+      status: 200,
+      body: 'Success',
+    };
+    if (success) {
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
