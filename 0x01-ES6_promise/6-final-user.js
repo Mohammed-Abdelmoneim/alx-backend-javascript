3c7f8ba6b7fe8f37fb69217466ef6af575520d2b
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-24 18:48:42 +0300

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(() => ({
-      status: Promise.status,
-      value: Promise.value,
-    }));
+    .then((result) => (
+      result.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
