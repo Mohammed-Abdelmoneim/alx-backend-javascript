803a70d2cfc900d6982479878f2353c1557c0478
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-24 16:02:49 +0300

    fix then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -15,7 +15,7 @@ export default function handleResponseFromAPI(promise) {
 const promise = handleResponseFromAPI();
 
 function handleResolve(val) {
-  console.log(val);
+  return val;
 }
 
 function handleReject(reason) {
