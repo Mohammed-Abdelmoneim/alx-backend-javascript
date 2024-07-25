30c019cf7a654056be217517ab1b0a2984d5b4be
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-25 18:52:59 +0300

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+    this.isValid();
+  }
+
+  isValid() {
+    if (typeof this._name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof this._length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(this._students)) {
+      throw new Error('Students must be an array');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLen) {
+    this._length = newLen;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newSt) {
+    this._students = newSt;
+  }
+}
