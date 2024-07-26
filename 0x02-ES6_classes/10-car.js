505cd0f9aa1abb32ea30879f6344fa9c20615f1c
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-26 21:05:40 +0300

    car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(barnd, motor, color) {
+    this._brand = barnd;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
