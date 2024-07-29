2cd796ae668fec94adffbe95c9b7f22738148b7e
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-29 22:40:31 +0300

    get_list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+  const arr = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+  return arr;
+}
+
+getListStudents();
