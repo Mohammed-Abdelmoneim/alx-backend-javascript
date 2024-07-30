584eb38b29c6a6e40a6f02e3e769d7b712a40926
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-30 16:03:04 +0300

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
   const arr = Array.from(set);
-
-  if (!startString) return '';
-
   const result = arr
     .filter((element) => String(element).startsWith(startString))
     .map((element) => String(element).slice(startString.length))
